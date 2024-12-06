1168020f4968f857123323c6847414187d6dfa12
pdnko <podenko.vladislav@gmail.com>
2024-12-06 15:06:47 +0300

    Icon component has been added

---
+++ components/icon/MagnifyingGlass.vue
@@ -0,0 +1,64 @@
+<template>
+	<svg
+		v-if="type === 'outlined'"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		:width="size"
+		:height="size">
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+	</svg>
+
+	<svg
+		v-else-if="type === 'solid'"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		fill="currentColor"
+		:width="size"
+		:height="size">
+		<path
+			fill-rule="evenodd"
+			d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z"
+			clip-rule="evenodd" />
+	</svg>
+
+	<svg
+		v-else-if="type === 'mini'"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 20 20"
+		fill="currentColor"
+		:width="size"
+		:height="size">
+		<path
+			fill-rule="evenodd"
+			d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z"
+			clip-rule="evenodd" />
+	</svg>
+
+	<svg
+		v-else-if="type === 'micro'"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 16 16"
+		fill="currentColor"
+		:width="size"
+		:height="size">
+		<path
+			fill-rule="evenodd"
+			d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+			clip-rule="evenodd" />
+	</svg>
+</template>
+
+<script setup lang="ts">
+import type { TIconType } from '~/types/icon';
+
+defineProps<{
+    type: TIconType
+    size: string
+}>();
+</script>
